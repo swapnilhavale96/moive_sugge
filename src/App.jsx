1ac860d961a72342342a3932f884e496620d55f4
Swapnil Havale <137410422+swapnilhavale96@users.noreply.github.com>
2025-03-13 13:59:34 +0530

    Your descriptive commit message

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,6 @@ function App() {
         <Route path="/about" element={<CreditsPage />} />
         <Route path="/video-player" element={<VideoPlayer />} />
       </Routes>
-      <div>
-      <h2>Meet the Creators</h2>
-      <img src="./assets/IMG_2432.JPG" alt="Profile Photo" />
-      <img src="./assets/raj.jpeg.JPG" alt="Profile Photo" />
-    </div>
     </>
   );
 }
